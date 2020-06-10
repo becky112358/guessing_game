5b000c375775f3ff1a23479df9348b2ad1274ca0
becky112358 <8b2@protonmail.com>
2020-06-10 09:09:10 +0300

    Receive an input from the user.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the guessing game!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
