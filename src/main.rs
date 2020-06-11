cee8500cddd968e449051f43348c6b2bf080e84b
becky112358 <8b2@protonmail.com>
2020-06-11 07:45:53 +0300

    Game now selects a random number.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Welcome to the guessing game!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("TEMPORARY: The secret number is {}", secret_number);
 
     println!("Please input your guess.");
 
